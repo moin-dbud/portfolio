e6e9fbf718dd5603e00f6366bb2c1161f95ecbd2
Moin sameer sheikh <132683286+moin-dbud@users.noreply.github.com>
2025-03-16 23:09:30 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,14 +23,15 @@ const Projects = () => {
       description: "An innovative platform developed for Innovate India 2025 Ideathon, featuring personalized learning paths.",
       tags: ["React", "AI", "Education"],
       link: "https://adaptivescholar.free.nf/?i=1"
-    },
-    {
-      icon: Shop,
-      title: "Tech Gadgets E-Commerce Website",
-      description: "This is a fully responsive Tech Gadgets E-Commerce Website built using HTML, Tailwind CSS, and JavaScript. The website offers a modern UI with dynamic product listings and smooth navigation for an enhanced user experience.",
-      tags: ["HTML", "Tailwind CSS", "Javascript"],
-      link: "https://techgadgetss.netlify.app/"
     }
+    // },
+    // {
+    //   icon: Shop,
+    //   title: "Tech Gadgets E-Commerce Website",
+    //   description: "This is a fully responsive Tech Gadgets E-Commerce Website built using HTML, Tailwind CSS, and JavaScript. The website offers a modern UI with dynamic product listings and smooth navigation for an enhanced user experience.",
+    //   tags: ["HTML", "Tailwind CSS", "Javascript"],
+    //   link: "https://techgadgetss.netlify.app/"
+    // }
   ];
 
   return (
