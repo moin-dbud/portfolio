fa7bc047a8cae19651deaf62bb705c773317f2b1
Moin sameer sheikh <132683286+moin-dbud@users.noreply.github.com>
2025-03-16 23:15:22 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bot, Camera, Laptop, ExternalLink } from 'lucide-react';
+import { Bot, Camera, Laptop, Cart, ExternalLink } from 'lucide-react';
 
 const Projects = () => {
   const projects = [
@@ -23,6 +23,13 @@ const Projects = () => {
       description: "An innovative platform developed for Innovate India 2025 Ideathon, featuring personalized learning paths.",
       tags: ["React", "AI", "Education"],
       link: "https://adaptivescholar.free.nf/?i=1"
+    },
+    {
+      icon: Cart,
+      title: "Tech Gadgets E-Commerce Website",
+      description: "This is a fully responsive Tech Gadgets E-Commerce Website built using HTML, Tailwind CSS, and JavaScript. The website offers a modern UI with dynamic product listings and smooth navigation for an enhanced user experience.",
+      tags: ["HTML", "Tailwind CSS", "Javascript"],
+      link: "https://techgadgetss.netlify.app/"
     }
   ];
 
